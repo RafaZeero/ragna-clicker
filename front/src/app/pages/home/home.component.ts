e2dbbd6b52490de8075f6bcab2f14be1a592745d
RafaZeero <rafaa99@gmail.com>
2023-07-11 20:07:38 -0300

    show monster on screen

--- front/src/app/pages/home/home.component.ts
+++ front/src/app/pages/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, inject } from '@angular/core';
+import { DomSanitizer } from '@angular/platform-browser';
 import { ApiService } from '@shared/services';
 
 @Component({
@@ -9,8 +10,16 @@ import { ApiService } from '@shared/services';
 })
 export default class HomeComponent implements OnInit {
   private readonly _api = inject(ApiService);
+  private readonly _domSanitizer = inject(DomSanitizer);
+
+  public image!: any;
 
   public ngOnInit(): void {
-    console.log(this._api.ragnarok);
+    // console.log(this._api.ragnarok);
+
+    // this._api.getMonster(1002).subscribe(console.log);
+    this._api.getImage(1002).subscribe(data => {
+      this.image = data;
+    });
   }
 }
