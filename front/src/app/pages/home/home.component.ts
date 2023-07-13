bba34e239c7d777ec8f0e3e742947d6b21f4c3e3
RafaZeero <rafaa99@gmail.com>
2023-07-12 23:41:58 -0300

    test monster reducing hp bar

--- front/src/app/pages/home/home.component.ts
+++ front/src/app/pages/home/home.component.ts
@@ -1,25 +1,35 @@
-import { Component, OnInit, inject } from '@angular/core';
-import { DomSanitizer } from '@angular/platform-browser';
+import { CommonModule } from '@angular/common';
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component, OnInit, inject } from '@angular/core';
 import { ApiService } from '@shared/services';
 
 @Component({
   standalone: true,
   selector: 'rag-home',
+  imports: [CommonModule],
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export default class HomeComponent implements OnInit {
   private readonly _api = inject(ApiService);
-  private readonly _domSanitizer = inject(DomSanitizer);
+  private readonly _cd = inject(ChangeDetectorRef);
 
-  public image!: any;
+  public image!: string;
+
+  public life = 100;
+
+  public player = {
+    damage: 5,
+  };
 
   public ngOnInit(): void {
-    // console.log(this._api.ragnarok);
-
-    // this._api.getMonster(1002).subscribe(console.log);
     this._api.getImage(1002).subscribe(data => {
       this.image = data;
+      this._cd.detectChanges();
     });
+  }
+
+  public attack() {
+    this.life -= this.player.damage;
   }
 }
