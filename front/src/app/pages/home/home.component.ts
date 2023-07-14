2f986a16407e79028d9dafc073e89f36fc38a972
RafaZeero <rafaa99@gmail.com>
2023-07-14 00:52:36 -0300

    create player service

--- front/src/app/pages/home/home.component.ts
+++ front/src/app/pages/home/home.component.ts
@@ -1,54 +1,10 @@
 import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, ChangeDetectorRef, Component, DestroyRef, OnInit, inject } from '@angular/core';
-import { ApiService } from '@shared/services';
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component, OnInit, inject } from '@angular/core';
+import { ApiService, PlayerService } from '@shared/services';
 import MapComponent from '../../components/map/map.component';
-import { Maps } from '@shared/models';
-import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
-import {
-  BehaviorSubject,
-  NEVER,
-  Subject,
-  combineLatestWith,
-  delay,
-  exhaustMap,
-  filter,
-  first,
-  iif,
-  last,
-  map,
-  of,
-  share,
-  shareReplay,
-  startWith,
-  switchMap,
-  take,
-  takeUntil,
-  takeWhile,
-  tap,
-} from 'rxjs';
+import { Maps, Player } from '@shared/models';
+import { BehaviorSubject, Observable, delay, filter, map, of } from 'rxjs';
 
-type Player = {
-  attributes: {
-    strength: number;
-  };
-  stats: {
-    damage: {
-      base: number;
-      weapon: number;
-    };
-  };
-  exp: {
-    current: {
-      base: number;
-      job: number;
-    };
-    toLevelUp: {
-      base: number;
-      job: number;
-    };
-  };
-  level: number;
-};
 @Component({
   standalone: true,
   selector: 'rag-home',
@@ -59,109 +15,76 @@ type Player = {
 })
 export default class HomeComponent implements OnInit {
   private readonly _api = inject(ApiService);
+  private readonly _playerService = inject(PlayerService);
   private readonly _cd = inject(ChangeDetectorRef);
-  private readonly _destroyRef = inject(DestroyRef);
 
-  public image!: string;
+  public image$!: Observable<string>;
 
+  // Mocked data
   public currentMap: Maps = 'prontera-south';
   public monsterData = {
     life: 100,
+    id: 10002,
+    exp: {
+      base: 10,
+      job: 7,
+    },
   };
 
+  // streams
   private _life$ = new BehaviorSubject<number>(this.monsterData.life);
   public life$ = this._life$.asObservable();
-  // public life$ = new Subject<number>();
-
-  private _showMonster$ = new BehaviorSubject<boolean>(true);
-  public showMonster$ = this._showMonster$.asObservable();
 
   public loadMonster$ = this.life$.pipe(
     // Map current life
     map(currentLife => currentLife > 0),
   );
 
+  // Reload monster after it dies
   public reloadMonster$ = this.loadMonster$.pipe(
     filter(data => !data),
-    delay(500),
+    // reload after 500 ms
+    delay(350),
   );
-
-  public player$ = new BehaviorSubject<Player>({
-    attributes: {
-      strength: 3,
-    },
-    level: 1,
-    exp: {
-      current: { base: 0, job: 0 },
-      toLevelUp: { base: 0, job: 0 },
-    },
-    stats: {
-      damage: {
-        base: 13,
-        weapon: 0,
-      },
-    },
-  });
-
-  public get player() {
-    return this.player$.value;
-  }
 
   public ngOnInit(): void {
     this._api.getImage(1002).subscribe(data => {
-      this.image = data;
+      // This way the empty image will not appear
+      this.image$ = of(data);
       this._cd.detectChanges();
     });
 
+    // Reload monster after it dies
     this.reloadMonster$.subscribe(this.giveExp);
   }
 
   public attack() {
     const currentLife = this._life$.value;
-    const damageDealt = this.player$.value.stats.damage.base + this.player$.value.stats.damage.base;
+    const damageDealt = this._playerService.calculateDamageDealt();
 
+    // Only do damage if there is hp
     if (currentLife > 0) {
       console.log('damageDealt: ', damageDealt);
       this._life$.next(currentLife - damageDealt);
     }
   }
 
-  public setWeaponDamage(player: Player, weaponDamage: number) {
-    // Remove any damage from previous weapon
-    this.unequipPreviousWeapon();
-    // Calculate new weapon damage
-    const newWeaponDamage = this.calculateDamage(player, weaponDamage);
-    const previousPlayerDamages = player.stats.damage;
-    // Update player damage
-    this.player$.next({ ...player, stats: { damage: { ...previousPlayerDamages, weapon: newWeaponDamage } } });
-    return newWeaponDamage;
-  }
-
-  public unequipPreviousWeapon() {
-    const player = this.player$.value;
-    const previousPlayerDamages = player.stats.damage;
-    this.player$.next({ ...player, stats: { damage: { ...previousPlayerDamages, weapon: 0 } } });
-  }
-
-  // Move to utils
-  private calculateDamage(player: Player, weaponDamage: number) {
-    // Add weaponType & weaponRefine to calc
-    const damage = player.attributes.strength * 2 + weaponDamage;
-
-    return damage;
-  }
-
   // Move to utils
   private giveExp = () => {
     console.log('exp!!');
     // Get exp
-    this.player$.next({
-      ...this.player$.value,
+    this._playerService.player = {
+      ...this._playerService.player,
       exp: {
-        ...this.player$.value.exp,
-        current: { base: this.player$.value.exp.current.base + 10, job: this.player$.value.exp.current.job + 7 },
+        ...this._playerService.player.exp,
+        current: {
+          base: this._playerService.player.exp.current.base + this.monsterData.exp.base,
+          job: this._playerService.player.exp.current.job + this.monsterData.exp.job,
+        },
       },
-    });
+    };
+
+    this._playerService.checkLevelUp();
 
     // New monster
     this._life$.next(100);
---
+++ front/src/app/shared/services/player/player.service.ts
@@ -0,0 +1,68 @@
+import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+import { defaultPlayer, expToLevelUp } from '@shared/constants';
+import { Player } from '@shared/models';
+import { calculateExpAfterLevelUp, calculateLevel } from '@shared/utils';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class PlayerService {
+  // Change to current user player
+  private _player$ = new BehaviorSubject<Player>(defaultPlayer);
+  public player$ = this._player$.asObservable();
+
+  public get player() {
+    return this._player$.value;
+  }
+
+  public set player(data: Player) {
+    this._player$.next(data);
+  }
+
+  public setWeaponDamage(weaponDamage: number) {
+    const player = this.player;
+    // Remove any damage from previous weapon
+    this.unequipPreviousWeapon();
+    // Calculate new weapon damage
+    const newWeaponDamage = this.calculateTotalDamage(player, weaponDamage);
+    const previousPlayerDamages = player.stats.damage;
+    // Update player damage
+    this.player = {
+      ...player,
+      stats: { damage: { ...previousPlayerDamages, weapon: newWeaponDamage } },
+    };
+    return newWeaponDamage;
+  }
+
+  public unequipPreviousWeapon() {
+    const player = this.player;
+    const previousPlayerDamages = player.stats.damage;
+    this.player = { ...player, stats: { damage: { ...previousPlayerDamages, weapon: 0 } } };
+  }
+
+  public checkLevelUp() {
+    const updatedPlayerLevel = calculateLevel(this.player);
+    const updatedExpLevel = calculateExpAfterLevelUp(this.player);
+    this.player = {
+      ...this.player,
+      level: { base: updatedPlayerLevel.baseLevel, job: updatedPlayerLevel.jobLevel },
+      exp: {
+        current: { base: updatedExpLevel.baseExp, job: updatedExpLevel.jobExp },
+        toLevelUp: {
+          base: expToLevelUp.base[updatedPlayerLevel.baseLevel],
+          job: expToLevelUp.job[updatedPlayerLevel.jobLevel],
+        },
+      },
+    };
+  }
+
+  // Move to utils
+  private calculateTotalDamage(player: Player, weaponDamage: number) {
+    // Add weaponType & weaponRefine to calc
+    const damage = player.attributes.strength * 2 + weaponDamage;
+    return damage;
+  }
+
+  public calculateDamageDealt = () => this.player.stats.damage.base + this.player.stats.damage.weapon;
+}
