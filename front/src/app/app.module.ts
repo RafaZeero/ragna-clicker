88fe7e6d14a5dea78de0434bd7438af756ce8f63
RafaZeero <rafaa99@gmail.com>
2023-07-13 23:07:13 -0300

    update app component

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -4,10 +4,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
+import { HudComponent } from './components/hud/hud.component';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule, HudComponent],
   providers: [],
   bootstrap: [AppComponent],
 })
