4761c8bfdef59d6ff227072b1ff2b9a6d8ca2c04
RafaZeero <rafaa99@gmail.com>
2023-07-14 00:52:58 -0300

    create player constant

---
+++ front/src/app/shared/constants/player.ts
@@ -0,0 +1,40 @@
+import { Player } from '@shared/models';
+
+export const defaultPlayer: Player = {
+  attributes: {
+    strength: 3,
+    agility: 5,
+    dexterity: 3,
+    inteligence: 4,
+    luck: 1,
+    vitality: 1,
+  },
+  level: {
+    base: 1,
+    job: 1,
+  },
+  exp: {
+    current: { base: 0, job: 0 },
+    toLevelUp: { base: 30, job: 270 },
+  },
+  stats: {
+    damage: {
+      base: 13,
+      weapon: 0,
+    },
+  },
+};
+
+export const expToLevelUp: Record<'base' | 'job', Record<string, number>> = {
+  base: {
+    // Level: expToLevelUp
+    '1': 30,
+    '2': 55,
+    '3': 80,
+  },
+  job: {
+    '1': 80,
+    '2': 121,
+    '3': 153,
+  },
+};
