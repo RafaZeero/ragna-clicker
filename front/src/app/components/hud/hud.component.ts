80f3f7507b892b7508a8ed71eb095cf8589573ec
RafaZeero <rafaa99@gmail.com>
2023-07-14 22:55:41 -0300

    update hud

--- front/src/app/components/hud/hud.component.ts
+++ front/src/app/components/hud/hud.component.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectionStrategy, Component, HostBinding } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PlayerComponent } from '../player/player.component';
 
@@ -11,13 +11,9 @@ import { PlayerComponent } from '../player/player.component';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class HudComponent {
-  @HostBinding('style.height.%')
-  public hostHeight = 75;
-
   public hudState = true;
 
   public toggleHud() {
     this.hudState = !this.hudState;
-    this.hostHeight = this.hudState ? 75 : 20;
   }
 }
