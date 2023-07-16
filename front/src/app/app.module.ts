e829de6ec3122e4dbcca9b333db0f06e12054f41
Rafael Lima - unvoid <rafael.lima@unvoid.io>
2023-07-16 10:23:18 -0300

    add drag and drop & boundary to it

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -5,10 +5,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
 import { HudComponent } from './components/hud/hud.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { DragDropModule } from '@angular/cdk/drag-drop';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, HttpClientModule, HudComponent],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule, HudComponent, BrowserAnimationsModule, DragDropModule],
   providers: [],
   bootstrap: [AppComponent],
 })
--- front/src/app/components/hud/hud.component.ts
+++ front/src/app/components/hud/hud.component.ts
@@ -1,16 +1,18 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PlayerComponent } from '../player/player.component';
-
+import { CdkDrag, CdkDragHandle } from '@angular/cdk/drag-drop';
 @Component({
   selector: 'rag-hud',
   standalone: true,
-  imports: [CommonModule, PlayerComponent],
+  imports: [CommonModule, PlayerComponent, CdkDrag, CdkDragHandle],
   templateUrl: './hud.component.html',
   styleUrls: ['./hud.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class HudComponent {
+  @Input({ required: true }) public boundaryParentContainer!: HTMLElement;
+
   public hudState = true;
 
   public toggleHud() {
