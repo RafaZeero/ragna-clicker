9a35d6caf621fe4f09f5d16a37c7b02b2845597d
Rafael Lima - unvoid <rafael.lima@unvoid.io>
2023-07-16 13:07:00 -0300

    improve hud component content

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -7,10 +7,19 @@ import { HttpClientModule } from '@angular/common/http';
 import { HudComponent } from './components/hud/hud.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { DragDropModule } from '@angular/cdk/drag-drop';
+import { PlayerComponent } from './components';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, HttpClientModule, HudComponent, BrowserAnimationsModule, DragDropModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    HudComponent,
+    BrowserAnimationsModule,
+    DragDropModule,
+    PlayerComponent,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
