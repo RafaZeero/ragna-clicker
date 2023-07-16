3e3a68d385e91a192962808a7918d98c940d900d
Rafael Lima - unvoid <rafael.lima@unvoid.io>
2023-07-16 12:56:11 -0300

    implement btn component in hud

--- front/src/app/components/hud/hud.component.ts
+++ front/src/app/components/hud/hud.component.ts
@@ -1,17 +1,19 @@
-import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
+import { ChangeDetectionStrategy, Component, HostListener, Input, ViewChild } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PlayerComponent } from '../player/player.component';
 import { CdkDrag, CdkDragHandle } from '@angular/cdk/drag-drop';
+import { ButtonRoundedComponent } from '../button-rounded';
 @Component({
   selector: 'rag-hud',
   standalone: true,
-  imports: [CommonModule, PlayerComponent, CdkDrag, CdkDragHandle],
+  imports: [CommonModule, PlayerComponent, CdkDrag, CdkDragHandle, ButtonRoundedComponent],
   templateUrl: './hud.component.html',
   styleUrls: ['./hud.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class HudComponent {
   @Input({ required: true }) public boundaryParentContainer!: HTMLElement;
+  @ViewChild('toggle') public toggle!: HTMLElement;
 
   public hudState = true;
 
