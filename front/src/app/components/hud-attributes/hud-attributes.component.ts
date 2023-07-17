e9269487578ed474712ef99044538651ca49071a
RafaZeero <rafaa99@gmail.com>
2023-07-17 10:30:05 -0300

    refactor to get player-attributes

--- front/src/app/components/hud-attributes/hud-attributes.component.ts
+++ front/src/app/components/hud-attributes/hud-attributes.component.ts
@@ -1,13 +1,13 @@
 import { ChangeDetectionStrategy, Component, Input, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { HudComponent, PlayerInfoComponent } from '@components';
+import { HudComponent, PlayerAttributesComponent } from '@components';
 import { DragDropModule } from '@angular/cdk/drag-drop';
 import { PlayerService } from '@shared/services';
 
 @Component({
   selector: 'rag-hud-attributes',
   standalone: true,
-  imports: [CommonModule, DragDropModule, HudComponent, PlayerInfoComponent],
+  imports: [CommonModule, DragDropModule, HudComponent, PlayerAttributesComponent],
   templateUrl: './hud-attributes.component.html',
   styleUrls: ['./hud-attributes.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
