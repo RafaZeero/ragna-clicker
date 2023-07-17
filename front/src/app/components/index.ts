9d6e856f8ad3180b2290d98e3f53abea28d9c111
RafaZeero <rafaa99@gmail.com>
2023-07-17 10:29:42 -0300

    add components to index

--- front/src/app/components/index.ts
+++ front/src/app/components/index.ts
@@ -4,7 +4,9 @@ export * from './monster';
 
 // Player stuff
 export * from './player-info';
+export * from './player-attributes';
 
 // Huds
 export * from './hud';
 export * from './hud-info';
+export * from './hud-attributes';
