22094abe690961bc41a0e58e55cebb1bff9a4c73
RafaZeero <rafaa99@gmail.com>
2023-07-18 09:44:55 -0300

    add comments to player model

--- front/src/app/shared/models/player.ts
+++ front/src/app/shared/models/player.ts
@@ -1,20 +1,42 @@
 import { Experience, Level } from './commons';
 
 export type Player = {
+  /**
+   * Player name
+   */
   name: string;
   // Todo: make classes with skills
   class: string;
+  /**
+   * How many points player have to add in attributes when level up
+   */
+  attributes_to_spend: number;
+  /**
+   * List of attributes to create you own build
+   */
   attributes: Attributes;
+  /**
+   * Secondary stats to do damage, resist spells, drop chance, etc
+   * TODO: Improve this
+   */
   stats: {
     damage: {
       base: number;
       weapon: number;
     };
   };
+  /**
+   * Experience points.
+   * Current experience points value
+   * Total experience needed to level up
+   */
   exp: {
     current: Experience;
     toLevelUp: Experience;
   };
+  /**
+   * Current level. Base level and Job level
+   */
   level: Level;
 };
 
@@ -30,3 +52,5 @@ export type Attributes = {
   dexterity: number;
   luck: number;
 };
+
+export type AttributesAliases = 'str' | 'agi' | 'vit' | 'int' | 'dex' | 'luk';
