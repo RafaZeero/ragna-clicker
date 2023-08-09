b3ea5f913960259206092a75576202629be5a34b
RafaZeero <rafaa99@gmail.com>
2023-08-09 16:47:47 -0300

    testing db

---
+++ back/src/db/index.ts
@@ -0,0 +1,31 @@
+import 'dotenv/config';
+import mysql, { ConnectionOptions, RowDataPacket } from 'mysql2';
+
+export const DB = () => {
+  const localDB: ConnectionOptions = {
+    host: 'localhost',
+    port: 3306,
+    user: 'root',
+    database: 'ragnadb',
+    password: 'password'
+  };
+
+  const dbUrl = process.env['DATABASE_URL'];
+  const development = process.env['DEVMODE'];
+
+  const connectionValue = development ? localDB : dbUrl;
+
+  // create the connection
+  const connection = mysql.createConnection(connectionValue as any);
+  // query database
+
+  connection.query<RowDataPacket[]>('SELECT * FROM monsters;', (_err, rows) => {
+    /**
+     * @rows: [ { test: 2 } ]
+     */
+
+    console.log(rows);
+  });
+
+  return connection;
+};
