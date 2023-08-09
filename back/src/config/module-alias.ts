27de1f969b757641b1ec2bd8cb4b39bfdc24926a
RafaZeero <rafaa99@gmail.com>
2023-08-09 16:46:31 -0300

    create module aliases

--- back/src/config/module-alias.ts
+++ back/src/config/module-alias.ts
@@ -4,3 +4,4 @@ import moduleAlias from 'module-alias';
 moduleAlias.addAlias('@controllers', join(__dirname, '../controllers'));
 moduleAlias.addAlias('@utils', join(__dirname, '../utils'));
 moduleAlias.addAlias('@interfaces', join(__dirname, '../interfaces'));
+moduleAlias.addAlias('@db', join(__dirname, '../db'));
