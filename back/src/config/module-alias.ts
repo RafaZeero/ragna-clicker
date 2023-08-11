a47ce6d1ce6d5b506975c41749c1a5e6aceaac30
RafaZeero <rafaa99@gmail.com>
2023-08-11 19:59:05 -0300

    add new module @<!-->repositories

--- back/src/config/module-alias.ts
+++ back/src/config/module-alias.ts
@@ -5,3 +5,4 @@ moduleAlias.addAlias('@controllers', join(__dirname, '../controllers'));
 moduleAlias.addAlias('@utils', join(__dirname, '../utils'));
 moduleAlias.addAlias('@interfaces', join(__dirname, '../interfaces'));
 moduleAlias.addAlias('@db', join(__dirname, '../db'));
+moduleAlias.addAlias('@repositories', join(__dirname, '../repositories'));
