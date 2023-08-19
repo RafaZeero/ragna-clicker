c251f8d6a77367fcfb60bbb298ba4ef853ce78d7
RafaZeero <rafaa99@gmail.com>
2023-08-19 15:08:23 -0300

    add date-fns

--- back/src/utils/jwt.ts
+++ back/src/utils/jwt.ts
@@ -1,7 +1,10 @@
 import { User } from '@interfaces';
+import { addDays } from 'date-fns/fp';
 import 'dotenv/config';
 import * as jwt from 'jsonwebtoken';
 import { uniqueId } from 'lodash';
 
 export const generateSafeJWT = (user: Omit<User, 'password'>): string =>
-  jwt.sign(JSON.stringify(user), uniqueId(process.env['SAFE_VALUE']));
+  jwt.sign(JSON.stringify({ user, expires_at: validFor(30) }), uniqueId(process.env['SAFE_VALUE']));
+
+const validFor = (time: number) => addDays(time)(new Date());
