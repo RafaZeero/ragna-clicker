4b39334268e041ccafd6a3ce9c5cbfc40be01626
RafaZeero <rafaa99@gmail.com>
2023-08-26 21:36:53 -0300

    testing sql with go

--- back-go/main.go
+++ back-go/main.go
@@ -1,10 +1,39 @@
 package main
 
 import (
-	"github.com/RafaZeero/ragna-clicker/router"
+	"database/sql"
+
+	// "github.com/RafaZeero/ragna-clicker/router"
+
+	_ "github.com/go-sql-driver/mysql"
 )
+
+func exec(db *sql.DB, sql string) sql.Result {
+	result, err := db.Exec(sql)
+	if err != nil {
+		panic(err)
+	}
+
+	return result
+}
 
 func main() {
 	// Init server
-	router.Initialize()
+	// router.Initialize()
+
+	db, err := sql.Open("mysql", "root:password@tcp(localhost:3306)/")
+	if err != nil {
+		panic(err)
+	}
+
+	defer db.Close()
+
+	exec(db, "create database if not exists testando1234")
+	exec(db, "use testando1234")
+	exec(db, "drop table if exists usuarios")
+	exec(db, `create table usuarios (
+		id integer auto_increment,
+		nome varchar(80),
+		PRIMARY KEY (id)
+	)`)
 }
